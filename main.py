3294b4ad09f54aa158b43fc7db2260edb3f9255e
Jhory Toro <jhordytoro49@gmail.com>
2022-03-25 16:40:24 -0500

    'commit1'

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+#Python
+from lib2to3.pytree import Node
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str 
+    age: int
+    hair_color: Optional[str] = None
+    married: Optional[bool] = None
+
 @app.get('/')
 def home():
-    return {'hello': 'world'}
+    return 'hola :3'
 
 @app.post('/person/new')
-def new_person():
-    pass
+def persona(person: Person = Body(...)):
+    return person
