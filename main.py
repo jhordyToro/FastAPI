2a25ac51f9115f540415e6e722f3e426d8f34752
Jhory Toro <jhordytoro49@gmail.com>
2022-03-25 18:38:01 -0500

    'query_parameters'

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 #Python
-from lib2to3.pytree import Node
+from optparse import Option
 from typing import Optional
+import fastapi
 
 #Pydantic
 from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body,Query
 
 app = FastAPI()
-
-class Person(BaseModel):
-    first_name: str
-    last_name: str 
+class persona(BaseModel):
+    first_name: str 
+    last_name: str
     age: int
     hair_color: Optional[str] = None
-    married: Optional[bool] = None
+    married: Optional[str] = None
 
 @app.get('/')
 def home():
-    return 'hola :3'
+    return 'hola xd'
 
 @app.post('/person/new')
-def persona(person: Person = Body(...)):
+def people(person: persona = Body(...)):
     return person
+
+@app.get('/person/deatils')
+def details(
+    name: Optional[str] = Query(None,min_length=1,max_length=50),
+    age: Optional[str] = Query(None,min_length=1,max_length=3)
+):
+    return {name: age}
+
